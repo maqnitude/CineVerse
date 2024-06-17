d40a648a426524bbca50cd122a16f20c4b6306a4
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-06-17 14:50:30 +0700

    feat: add component base class to use with mediator

---
+++ Views/Component.cs
@@ -0,0 +1,19 @@
+﻿using CineVerse.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineVerse.Views
+{
+    public class Component
+    {
+        protected IMediator _mediator;
+
+        public void SetMediator(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}

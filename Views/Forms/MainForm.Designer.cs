c85fabed1861652d33c1206a38f621d3f1adc8d1
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-06-17 10:15:44 +0700

    feat: add initial MovieCard control

--- Views/Forms/MainForm.Designer.cs
+++ Views/Forms/MainForm.Designer.cs
@@ -28,57 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            button1 = new Button();
-            panel2 = new Panel();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.AutoScroll = true;
-            panel1.BackColor = SystemColors.GradientInactiveCaption;
-            panel1.Location = new Point(62, 35);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1098, 345);
-            panel1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            button1.BackColor = SystemColors.ActiveCaption;
-            button1.Location = new Point(516, 696);
-            button1.Name = "button1";
-            button1.Size = new Size(166, 64);
-            button1.TabIndex = 1;
-            button1.Text = "Load Movies";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // panel2
-            // 
-            panel2.AutoScroll = true;
-            panel2.BackColor = SystemColors.ControlDark;
-            panel2.Location = new Point(62, 401);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(1098, 277);
-            panel2.TabIndex = 2;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1226, 772);
-            Controls.Add(panel2);
-            Controls.Add(button1);
-            Controls.Add(panel1);
             Name = "MainForm";
             Text = "MainForm";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel panel1;
-        private Button button1;
-        private Panel panel2;
     }
 }
--- Views/Forms/MainForm.cs
+++ Views/Forms/MainForm.cs
@@ -17,6 +17,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CineVerse.Views.UserControls;
 
 namespace CineVerse.Forms
 {
@@ -27,76 +28,6 @@ namespace CineVerse.Forms
         public MainForm()
         {
             InitializeComponent();
-            _moviePanels = new Dictionary<PictureBox, Movie>();
-        }
-
-        private void LoadMovies()
-        {
-            using (var context = new AppDbContext())
-            {
-                var movies = context.Movies.Take(10).ToList();
-                foreach (var movie in movies)
-                {
-                    Panel movieContainer = new Panel();
-                    PictureBox moviePoster = new PictureBox();
-                    moviePoster.Image = Image.FromFile(movie.PosterPath);
-                    moviePoster.SizeMode = PictureBoxSizeMode.StretchImage;
-                    Label movieTitle = new Label();
-                    movieTitle.Text = movie.Title;
-                    movieContainer.Controls.Add(movieTitle);
-                    movieTitle.Dock = DockStyle.Bottom;
-                    movieContainer.Controls.Add(moviePoster);
-                    moviePoster.Dock = DockStyle.Fill;
-
-                    panel1.Controls.Add(movieContainer);
-                    movieContainer.Dock = DockStyle.Left;
-
-                    _moviePanels[moviePoster] = movie;
-                    moviePoster.Click += MovieContainer_Click;
-                }
-            }
-        }
-
-        private void MovieContainer_Click(object sender, EventArgs e)
-        {
-            panel2.Controls.Clear();
-
-            var pictureBox = (PictureBox)sender;
-            var movie = _moviePanels[pictureBox];
-
-            using (var context = new AppDbContext())
-            {
-                var credits = context.Credits.Include(c => c.Person).Where(c => c.MovieId == movie.Id && c.Type == "cast").OrderBy(c => c.Order).Take(10).ToList();
-                foreach (Credit credit in credits)
-                {
-                    Panel creditContainer = new Panel();
-                    Label creditLabel = new Label();
-                    creditLabel.Text = $"Name: {credit.Person.Name}";
-                    PictureBox creditPicture = new PictureBox();
-                    if (credit.Person.ProfilePath != null)
-                    {
-                        creditPicture.Image = Image.FromFile(credit.Person.ProfilePath);
-                        creditPicture.SizeMode = PictureBoxSizeMode.StretchImage;
-                    }
-                    else
-                    {
-                        creditPicture.BackColor = Color.Black;
-                    }
-
-                    creditContainer.Controls.Add(creditLabel);
-                    creditLabel.Dock = DockStyle.Bottom;
-                    creditContainer.Controls.Add(creditPicture);
-                    creditPicture.Dock = DockStyle.Fill;
-
-                    panel2.Controls.Add(creditContainer);
-                    creditContainer.Dock = DockStyle.Left;
-                }
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            LoadMovies();
         }
     }
 }

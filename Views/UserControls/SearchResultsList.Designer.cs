005846efa9f5398a0d6b69b63f40003d9ed0dd42
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-06-29 09:08:55 +0700

    feat(ui): improve displayed search results UI

--- Views/UserControls/SearchResultsList.Designer.cs
+++ Views/UserControls/SearchResultsList.Designer.cs
@@ -29,29 +29,62 @@
         private void InitializeComponent()
         {
             pnlResults = new Panel();
+            panel1 = new Panel();
+            label1 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // pnlResults
             // 
             pnlResults.AutoScroll = true;
+            pnlResults.AutoSize = true;
+            pnlResults.BackColor = Color.FromArgb(82, 95, 109);
             pnlResults.Dock = DockStyle.Fill;
             pnlResults.Location = new Point(0, 0);
             pnlResults.Name = "pnlResults";
-            pnlResults.Size = new Size(318, 437);
+            pnlResults.Size = new Size(250, 0);
             pnlResults.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(54, 68, 82);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 32);
+            panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Dock = DockStyle.Fill;
+            label1.ForeColor = Color.FromArgb(173, 196, 214);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(250, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Search ended? Film may be unavailable.";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // SearchResultsList
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
             Controls.Add(pnlResults);
+            Controls.Add(panel1);
+            MaximumSize = new Size(0, 200);
             Name = "SearchResultsList";
-            Size = new Size(318, 437);
+            Size = new Size(250, 32);
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel pnlResults;
+        private Panel panel1;
+        private Label label1;
     }
 }
